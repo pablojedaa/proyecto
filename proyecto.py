b33ef6d38ebd6ef1d7b98cba8ea5f2ec7f825b82
Isaacsanz128 <isangar0512@g.educaand.es>
2026-03-18 11:13:59 +0100

    insertamos los valores en la tabla manager

--- proyecto.py
+++ proyecto.py
@@ -6,3 +6,9 @@ cursor = conexion.cursor()
 cursor.execute('''INSERT INTO Manager (id_manager, nombre, edad) VALUES(?,?,?)''', (1 ,"Pellegrini", 72))
 cursor.execute('''INSERT INTO Manager (id_manager, nombre, edad) VALUES(?,?,?)''', (2 ,"Pepe Mel", 63))
 cursor.execute('''INSERT INTO Manager (id_manager, nombre, edad) VALUES(?,?,?)''', (3 ,"Setién", 67))
+
+#insertamos los valores en la tabla marca
+cursor.execute('''INSERT INTO Marcas (id_marca, nombre, dinero) VALUES(?,?,?)''', (1 ,"Kappa", 36000000))
+cursor.execute('''INSERT INTO Marcas (id_marca, nombre, dinero) VALUES(?,?,?)''', (2 ,"Adidas", 50000000))
+cursor.execute('''INSERT INTO Marcas (id_marca, nombre, dinero) VALUES(?,?,?)''', (3 ,"Macron", 14000000))
+
