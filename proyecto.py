e72b9f10ada1c3eb36645e729b742cea0475d64d
pablojedaa_ <pojegar942@g.educaand.es>
2026-03-20 10:38:34 +0100

    borramos las insercciones para que no sean periódicas a la hora de ejecutar el programa

--- proyecto.py
+++ proyecto.py
@@ -2,17 +2,3 @@
 import sqlite3
 conexion = sqlite3.connect('betis.db')
 cursor = conexion.cursor()
-#insertamos los valores en la tabla manager
-cursor.execute('''INSERT INTO Manager (id_manager, nombre, edad) VALUES(?,?,?)''', (1 ,"Pellegrini", 72))
-cursor.execute('''INSERT INTO Manager (id_manager, nombre, edad) VALUES(?,?,?)''', (2 ,"Pepe Mel", 63))
-cursor.execute('''INSERT INTO Manager (id_manager, nombre, edad) VALUES(?,?,?)''', (3 ,"Setién", 67))
-
-#insertamos los valores en la tabla marca
-cursor.execute('''INSERT INTO Marcas (id_marca, nombre, dinero) VALUES(?,?,?)''', (1 ,"Kappa", 36000000))
-cursor.execute('''INSERT INTO Marcas (id_marca, nombre, dinero) VALUES(?,?,?)''', (2 ,"Adidas", 50000000))
-cursor.execute('''INSERT INTO Marcas (id_marca, nombre, dinero) VALUES(?,?,?)''', (3 ,"Macron", 14000000))
-
-#insertamos los valores en la tabla jugadores
-cursor.execute('''INSERT INTO Marcas (id_jugador, nombre, edad, id_manager, id_marca) VALUES(?,?,?)''', (1 ,"Joaquín", 44, 1, 1))
-cursor.execute('''INSERT INTO Marcas (id_jugador, nombre, edad, id_manager, id_marca) VALUES(?,?,?)''', (2 ,"Rubén Castro", 44, 2, 3))
-cursor.execute('''INSERT INTO Marcas (id_jugador, nombre, edad, id_manager, id_marca) VALUES(?,?,?)''', (1 ,"Fabián", 29, 3, 2))
